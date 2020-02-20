739a10a5e371bbbb2e32b5648fefe6e4c9befe3c
dom <marsonowski14@gmail.com>
2020-02-20 02:24:51 +0100

    Usunąłem Repository i Service, Wyświetlam text z Controllera

--- src/main/java/pl/adrian/gonzo2020/GonzoController.java
+++ src/main/java/pl/adrian/gonzo2020/GonzoController.java
@@ -1,11 +1,15 @@
+package pl.adrian.gonzo2020;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GonzoController {
 
-    @GetMapping ("/")
-    public String get(){
+    @GetMapping("/")
+    public String get(Model model) {
+        model.addAttribute("name", "Adrian");
         return "index";
     }
 }
---
+++ src/test/java/pl/adrian/gonzo2020/Gonzo2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adrian.gonzo2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gonzo2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
